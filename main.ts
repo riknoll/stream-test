cab9439f96549e61aac7e5c2c40f1a0081c97b29
shakao <34112083+shakao@users.noreply.github.com>
2020-04-07 14:41:44 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
- 
+let mySprite: Sprite = null
+game.onUpdateInterval(5000, function () {
+    mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . c c c c c c . . . . 
+. . . . . c c c c c c c c . . . 
+. . . . c c c c d c c d c c . . 
+. . . c c c c d c c c c d c . . 
+. . . c c c c c c c c c c c . . 
+. . . c c c c c c c c c c c c . 
+. . . c c c c c c d d c c c c . 
+. . . c c c c c c d d c c c c . 
+. . c c c c c c c c c c c c c . 
+. . c c c c c c c c c c c c . . 
+. . c c c c c c c c c c c c . . 
+. . c c c . . c c c . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Food)
+})
